42875e9d50ea7aeba098b06cb46f36639c4ef9da
henryfogacas <henryfogacas@gmail.com>
2024-01-27 19:03:01 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import Cesta from '../src/telas/Cesta';
+import { StatusBar, SafeAreaView } from 'react-native'
+
+const App = () => {
+  return <SafeAreaView>
+    <StatusBar />
+    <Cesta />
+  </SafeAreaView>
+}
+
+export default App;
