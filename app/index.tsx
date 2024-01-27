47d88cef38372e953b735d30c6fd39c4683dc7ce
henryfogacas <henryfogacas@gmail.com>
2024-01-27 19:04:48 -0300

    Adicionando fonte do google fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
 import Cesta from '../src/telas/Cesta';
-import { StatusBar, SafeAreaView } from 'react-native'
+import { StatusBar, SafeAreaView, View } from 'react-native'
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 const App = () => {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  })
+  if (!fonteCarregada) {
+    return <View></View>
+  }
+
   return <SafeAreaView>
     <StatusBar />
     <Cesta />
